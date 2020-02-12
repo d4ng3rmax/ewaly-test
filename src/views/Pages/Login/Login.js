49ffad9de324df0f44bd9842e56035a07d40ab12
G.Silveira <gabriel.ch23@gmail.com>
2020-02-12 00:24:28 -0300

    pushing ewally test

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
 class Login extends Component {
@@ -8,13 +7,13 @@ class Login extends Component {
       <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="8">
+            <Col md="4">
               <CardGroup>
                 <Card className="p-4">
                   <CardBody>
                     <Form>
                       <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
+                      <p className="text-muted">Entrar com usuário e senha</p>
                       <InputGroup className="mb-3">
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
@@ -33,25 +32,10 @@ class Login extends Component {
                       </InputGroup>
                       <Row>
                         <Col xs="6">
-                          <Button color="primary" className="px-4">Login</Button>
-                        </Col>
-                        <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
+                          <Button color="primary" className="px-4">Entrar</Button>
                         </Col>
                       </Row>
                     </Form>
-                  </CardBody>
-                </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Link to="/register">
-                        <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
-                      </Link>
-                    </div>
                   </CardBody>
                 </Card>
               </CardGroup>
--- src/views/Pages/Page404/Page404.js
+++ src/views/Pages/Page404/Page404.js
@@ -10,8 +10,8 @@ class Page404 extends Component {
             <Col md="6">
               <div className="clearfix">
                 <h1 className="float-left display-3 mr-4">404</h1>
-                <h4 className="pt-3">Oops! You're lost.</h4>
-                <p className="text-muted float-left">The page you are looking for was not found.</p>
+                <h4 className="pt-3">Oops! Caminho errado... =/</h4>
+                <p className="text-muted float-left">A rota escolhida não foi encontrada.</p>
               </div>
               <InputGroup className="input-prepend">
                 <InputGroupAddon addonType="prepend">
@@ -19,9 +19,9 @@ class Page404 extends Component {
                     <i className="fa fa-search"></i>
                   </InputGroupText>
                 </InputGroupAddon>
-                <Input size="16" type="text" placeholder="What are you looking for?" />
+                <Input size="16" type="text" placeholder="O que você está procurando?" />
                 <InputGroupAddon addonType="append">
-                  <Button color="info">Search</Button>
+                  <Button color="info">Pesquisar</Button>
                 </InputGroupAddon>
               </InputGroup>
             </Col>
